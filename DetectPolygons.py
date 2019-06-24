70a4377b747c60bd89180b60d1ab1fabb673596a
Pramod <38784314+pramodlradder@users.noreply.github.com>
2019-06-24 20:49:23 +0530

    Update and rename opencv Polygons.py to DetectPolygons.py

--- DetectPolygons.py
+++ DetectPolygons.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import cv2
 
@@ -13,16 +7,13 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 ret,thresh = cv2.threshold(gray,200,255,0)#threshould value less than that will be 0 else max value(ret) 2 nd parameter
 #print(ret,thresh)
-cv2.imshow("threasimage",thresh)
-cv2.imshow("gray image",gray)
-cv2.imshow("origanal image",img)
 contours,h = cv2.findContours(thresh,cv2.RETR_TREE,2)#tree hirarchical numbering  h number of counters 
 copy = img.copy()
 #3rd parameter contour number
 #print(contours)
 #print(h)
 
-#image c2.png for circle
+#image polygon.png for circle
 circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 2, 1)
 #print(circles)
 for i in circles[0,:]:
@@ -47,13 +38,16 @@ for cnt in contours:
         print("hexagon")
         cv2.drawContours(copy, [cnt],0, (25,55,0), 10)
     
-        
-cv2.imshow("cccccccimage",copy)
+
+cv2.imshow("origanal image",img)
+
+cv2.imshow("gray image",gray)
+cv2.imshow("threasimage",thresh)        
+cv2.imshow("image",copy)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
-# In[ ]:
 
 
 
